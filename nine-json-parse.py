4ab579d8f67253038fd2c01649c7075d7fefc52a
Lewis Smith <lewisgeorgesmith2001@gmail.com>
2022-03-07 21:48:07 +1100

    untested preliminary allowances made for valueError exceptions

--- nine-json-parse.py
+++ nine-json-parse.py
@@ -5,17 +5,25 @@ def filter(data):
     #data = json.load(f) - loading from the file
     responseList = []
     #initialising list variable
-    for i in data['payload']:
-         #for the data
-        if i.get('drm') == True and i.get('episodeCount') > 0:
-            #Checks that the given criteria are both true
-            entry = [i.get('image'), i.get('slug'), i.get('title')]
-            #sets the entry as a list - change this to dict for JSON
-            responseList.append(entry)
-            #adds the entry to the list of correct data
-            response = json.dumps(responseList)
-            # Change dumps to dump, as that will allow for output to JSON
-    return json.dumps(response)
+    try:
+        for i in data['payload']:
+            #for the data
+            if i.get('drm') == True and i.get('episodeCount') > 0:
+                #Checks that the given criteria are both true
+                entry = [i.get('image'), i.get('slug'), i.get('title')]
+                #sets the entry as a list - change this to dict for JSON
+                responseList.append(entry)
+                #adds the entry to the list of correct data
+                response = json.dumps(responseList)
+                # Change dumps to dump, as that will allow for output to JSON
+        return json.dumps(response)
+
+    except ValueError:
+        return json.dumps(
+            {
+            "error": "Could not decode request: JSON parsing failed"
+            }
+        )
 
 #requests.post('https://nine-coding-challenge-lsmith.herokuapp.com/', json=json.dump("new.csv", f))
 url = requests.get("https://mocki.io/v1/39cd8b16-b3fc-4929-a0b2-fd3565d7f08d")
