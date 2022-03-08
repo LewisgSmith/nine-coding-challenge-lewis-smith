a296300d1623352b02a08568371216839b70bf26
Lewis Smith <lewisgeorgesmith2001@gmail.com>
2022-03-08 22:55:28 +1100

    formatting fixed and correct returns checked

--- nine-json-parse.py
+++ nine-json-parse.py
@@ -5,17 +5,16 @@ import requests
 returned_data = {}
 
 def filter(data):
-    j = 0
+    #j = 0
+    responseList = []
     for i in data['payload']:
         if i.get('drm') == True and i.get('episodeCount') > 0:
-            j+=1
-            testList = [str(i.get('image')), i.get('slug'), i.get('title')]
-            testDict = dict.fromkeys(testList)
-            #if i.get('drm') == True and i.get('episodeCount') > 0:
-            returned_data[j] = testDict
-    #print(json.dumps(data['payload']))
-    testerList = ["response:", returned_data]
-    print(json.dumps(testerList))
+            dict = {"image": i.get('image').get("showImage"), "slug": i.get('slug'), "title": i.get('title')}
+            responseList.append(dict)
+    returnDict = {"response": responseList}
+    return json.dumps(returnDict, indent=4)
+
+
 url = requests.get("https://mocki.io/v1/05bdf9aa-28f8-4e75-9698-29ccf8cbda3a", timeout=None)
 output = json.loads(url.text)
 print(filter(output))
