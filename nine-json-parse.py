b16fd7e78ab0aedfe7bbb08830242b30282d31aa
Lewis Smith <lewisgeorgesmith2001@gmail.com>
2022-03-08 22:27:59 +1100

    testing messy new solution using dict.fromkeys to convert list to dict

--- nine-json-parse.py
+++ nine-json-parse.py
@@ -1,31 +1,21 @@
 import json
+from logging.handlers import SYSLOG_UDP_PORT
 import requests
 
+returned_data = {}
+
 def filter(data):
-    #data = json.load(f) - loading from the file
-    responseList = []
-    #initialising list variable
-    try:
-        for i in data['payload']:
-            #for the data
-            if i.get('drm') == True and i.get('episodeCount') > 0:
-                #Checks that the given criteria are both true
-                entry = [i.get('image'), i.get('slug'), i.get('title')]
-                #sets the entry as a list - change this to dict for JSON
-                responseList.append(entry)
-                #adds the entry to the list of correct data
-                response = json.dumps(responseList)
-                # Change dumps to dump, as that will allow for output to JSON
-        return json.dumps(response, indent=4)
-
-    except ValueError:
-        return json.dumps(
-            {
-            "error": "Could not decode request: JSON parsing failed"
-            }
-        )
-
-#requests.post('https://nine-coding-challenge-lsmith.herokuapp.com/', json=json.dump("new.csv", f))
-url = requests.get("https://mocki.io/v1/39cd8b16-b3fc-4929-a0b2-fd3565d7f08d", timeout=None)
+    j = 0
+    for i in data['payload']:
+        if i.get('drm') == True and i.get('episodeCount') > 0:
+            j+=1
+            testList = [str(i.get('image')), i.get('slug'), i.get('title')]
+            testDict = dict.fromkeys(testList)
+            #if i.get('drm') == True and i.get('episodeCount') > 0:
+            returned_data[j] = testDict
+    #print(json.dumps(data['payload']))
+    testerList = ["response:", returned_data]
+    print(json.dumps(testerList))
+url = requests.get("https://mocki.io/v1/05bdf9aa-28f8-4e75-9698-29ccf8cbda3a", timeout=None)
 output = json.loads(url.text)
 print(filter(output))
